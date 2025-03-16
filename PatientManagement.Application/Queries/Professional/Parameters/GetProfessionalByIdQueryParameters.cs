e8949da4bda4351bf3ff8b11365355176962f94f
Ifunanya <williamifunanya@gmail.com>
2025-03-16 08:03:04 +0100

    Add GetProfessionalByIdQueryParameters

---
+++ PatientManagement.Application/Queries/Professional/Parameters/GetProfessionalByIdQueryParameters.cs
@@ -0,0 +1,15 @@
+﻿
+namespace PatientManagement.Application.Queries.Professional.Parameters
+{
+    using Common.Contracts;
+
+    public class GetProfessionalByIdQueryParameters : IQueryParameters
+    {
+        public GetProfessionalByIdQueryParameters(Guid professionalId)
+        {
+            ProfessionalId = professionalId;
+        }
+
+        public Guid ProfessionalId { get; }
+    }
+}
